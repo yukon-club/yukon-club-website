f8f9308c78d4a6339f59155c5f2a13d36743ef3a
Lars Knitel <110813764+lars-ah@users.noreply.github.com>
2025-12-02 20:54:57 +0100

    Hide newsletter component temporarily until backend is ready

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Hero from './Imageblock';
-import Newsletter from './Newsletter';
+// import Newsletter from './Newsletter'; // Temporarily hidden - backend not ready yet
 
 function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
       <Hero />
-      <Newsletter />
+      {/* <Newsletter /> */}
     </main>
   );
 }
