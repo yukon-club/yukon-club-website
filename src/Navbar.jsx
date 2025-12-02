d7ce5859bc7d6d4aa7915d1f9d67b412b06bc8c7
Lars Knitel <110813764+lars-ah@users.noreply.github.com>
2025-12-02 09:19:54 +0100

    Fix: remove unused imports from Navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
-import { FaBars, FaTimes } from 'react-icons/fa';
 import SocialMediaIcons from "./SocialMediaIcons";
 
 const Navbar = () => {
